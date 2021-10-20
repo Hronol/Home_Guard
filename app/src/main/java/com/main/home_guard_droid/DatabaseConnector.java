8b149de5570681f02fae370744bd1d1f29cbe843
Leszek Hronowski <hronol123@gmail.com>
2021-10-20 22:29:11 +0200

    zmiany w bazie

--- app/src/main/java/com/main/home_guard_droid/DatabaseConnector.java
+++ app/src/main/java/com/main/home_guard_droid/DatabaseConnector.java
@@ -4,29 +4,37 @@ import static android.content.ContentValues.TAG;
 
 import android.util.Log;
 import com.google.firebase.database.*;
+import com.google.firebase.firestore.auth.User;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class DatabaseConnector {
 
-    private DatabaseReference mDatabase; // should be final, probably
+    private static final String TAG = "ReadAndWriteSnippets";
 
-    public DatabaseConnector()
-    {
+    private DatabaseReference mDatabase;
+
+    public ReadAndWriteSnippets(DatabaseReference database) {
         mDatabase = FirebaseDatabase.getInstance().getReference();
     }
 
-    ValueEventListener postListener = new ValueEventListener() {
-        @Override
-        public void onDataChange(DataSnapshot dataSnapshot) {
-            // Get Post object and use the values to update the UI
-            //Post post = dataSnapshot.getValue(Post.class);
-            // ..
-        }
+    private void addPostEventListener(DatabaseReference mPostReference) {
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                Post post = dataSnapshot.getValue(Post.class);
+                // ..
+            }
 
-        @Override
-        public void onCancelled(DatabaseError databaseError) {
-            // Getting Post failed, log a message
-            Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
-        }
-    };
-    //mPostReference.addValueEventListener(postListener);
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+            }
+        };
+        mPostReference.addValueEventListener(postListener);
+        // [END post_value_event_listener]
+    }
 }
